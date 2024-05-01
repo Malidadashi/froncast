34c1191dfa0a763b13332d2c86190688cd701970
mad <mad@yahoo.com>
2024-05-01 15:32:35 +0330

    1th (4th) commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import Simple_Dev from './Components/Simple-Dev/Simple_Dev'
 const App = () => {
   return (
     <div>
 <Navbar/>
+<Simple_Dev/>
     </div>
   )
 }
--- src/Components/Simple-Dev/Simple_Dev.jsx
+++ src/Components/Simple-Dev/Simple_Dev.jsx
@@ -1,36 +1,45 @@
-import React from 'react'
-import "./simple-dev.css"
-import react from '../../assets/react.jpg'
+import React from "react";
+import "./simple-dev.css";
+import uwu from "../../assets/uwu.png";
+import { MdOutlineEditNote } from "react-icons/md";
+import { CiVideoOn } from "react-icons/ci";
+import { IoWalletOutline } from "react-icons/io5";
+import { IoMdTime } from "react-icons/io";
+import { RiMenu3Line } from "react-icons/ri";
+import { BsChatQuote } from "react-icons/bs";
 const Simple_Dev = () => {
   return (
-    <div>
+    <div className="simple-dev">
       <div>
         <h1></h1>
-        <p></p>
+        <p>
+          در فرانت کست می توانید مهارت های برنامه نویسی خود را تقویت کنید و به
+          یک توسعه دهنده حرفه ای تبدیل شوید
+          
+          
+        </p>
       </div>
 
-  
       <div>
-        <button></button>
+        <button>شروع یادگیری</button>
       </div>
 
       <div>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>یادگیری با انجام تمرین <MdOutlineEditNote/></li>
+          <li>ویدیوهای کوتاه و با کیفیت<IoMdTime/></li>
+          <li>ضمانت بازگشت وجه<IoWalletOutline/></li>
+          <li>به روز رسانی رایگان<CiVideoOn/></li>
+          <li>سرفصل‌های جامع و دقیق<RiMenu3Line/></li>
+          <li>پشتیبانی دوره‌ها<BsChatQuote/></li>
         </ul>
       </div>
 
-
       <div>
-<img src={'react'} alt='reactpicture'/>
+        <img src={uwu} alt="uwu" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Simple_Dev
+export default Simple_Dev;
