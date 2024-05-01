6459daff704cc0b0a365b471b4818544fe09f73d
mad <mad@yahoo.com>
2024-05-01 11:31:48 +0330

    1th commit

---
+++ frontcast/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './navbar.css'
+import '../../assets/frontcast-logo-top.png'
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+<img src={frontcast-logo-top} alt='frontcastlogo'/>
+asd
+      </div>
+    
+    <div>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default Navbar
