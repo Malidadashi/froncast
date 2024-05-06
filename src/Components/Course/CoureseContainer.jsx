0b11aa06f397aa0af22f00807c8a6df9f1165c8d
mad <mad@yahoo.com>
2024-05-06 14:33:53 +0330

    a

--- src/Components/Course/CoureseContainer.jsx
+++ src/Components/Course/CoureseContainer.jsx
@@ -2,34 +2,38 @@ import React from "react";
 // import CourseItem from "./CourseItem";
 import "./CourseContainer.css";
 import nextjs from "../../assets/nextjs.jpg";
-import js_advanced_concepts from '../../assets/js_advanced_concepts.jpg'
+import js_advanced_concepts from "../../assets/js_advanced_concepts.jpg";
+import typescript from '../../assets/typescript.jpg'
 import CourseItem from "./CourseItem";
-
 
 const CoureseContainer = () => {
   const courseItem = [
     {
       courseTitle: "آموزش nextjs",
-      price: '850 هزار تومان',
-      image :{nextjs}
+      price: "850 هزار تومان",
+      image: nextjs,
     },
 
     {
       courseTitle: "آموزش React",
-      price: '500 هزار تومان',
-      image:{js_advanced_concepts}
+      price: "500 هزار تومان",
+      image: js_advanced_concepts,
     },
     {
       courseTitle: "آموزش Css",
       price: "رایگان",
+      image:typescript
     },
   ];
   return (
     <section className="course-container">
-   
       {courseItem.map((course) => {
         return (
-          <CourseItem courseTitle={course.courseTitle} price={course.price} imagesrc={course.image}/>
+          <CourseItem
+            courseTitle={course.courseTitle}
+            price={course.price}
+            imagesrc={course.image}
+          />
         );
       })}
     </section>
